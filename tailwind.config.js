c18305e36e9f833cc5e2f369b9cac883d7a77704
mohammadsaeediaf <alisaadati18000@gmail.com>
2024-06-17 12:42:39 +0330

    Add social link card,challenge from FrontEnd Mentor

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#141414",
+        secondary: "#1f1f1f",
+        green: "#c5f82a",
+        grey: "#333",
+        white: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
